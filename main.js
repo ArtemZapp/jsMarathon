507691e44a067d9bb910455afba161f6ca0b402e
Artem Zapp <artmore111@ukr.net>
2020-09-08 17:52:17 +0300

    add -storm effect -sounds -alertBox

--- main.js
+++ main.js
@@ -4,24 +4,11 @@ const $kick_bonus = document.getElementById('kick-bonus');
 const $textFight = document.getElementById('textFight');
 const $character = document.getElementsByClassName('pokemon character')[0].childNodes;
 const $enemy = document.getElementsByClassName('pokemon enemy')[0].childNodes;
-let isResizeble = false;
-let i;
-
-let audio = new Audio();
-audio.preload = 'auto';
-audio.src = 'assets/wow.mp3';
-
-function boxThis() {
-	$textFight.parentElement.style.opacity = '1';
-	$textFight.parentElement.style.padding = '20px';
-	audio.play();
-	setTimeout(function(){ $textFight.parentElement.style.display = 'none'; }, 3000);
-}
-
-console.log($character[5]);
+const modal = document.getElementById("myModal");
+const modalText = document.getElementById("modal-text");
 
 const character = {
-	name: $character[5],
+	name: $character[5].getElementsByTagName("h2")[0].innerText,
 	defaultHP: 100,
 	damageHP: 100,
 	elHP: document.getElementById('health-character'),
@@ -29,24 +16,62 @@ const character = {
 }
 
 const enemy = {
-	name: $enemy,
+	name: $enemy[5].getElementsByTagName("h2")[0].innerText,
 	defaultHP: 100,
 	damageHP: 100,
 	elHP: document.getElementById('health-enemy'),
 	elProgressbar: document.getElementById('progressbar-enemy'),
 }
 
+let isResizeble = false;
+let i;
+let fail_audio = new Audio();
+let win_audio = new Audio();
+win_audio.preload = 'auto';
+win_audio.src = 'assets/wow.mp3';
+fail_audio.preload = 'auto';
+fail_audio.src = 'assets/wasted.mp3';
+
+function showBoxWin() {
+	$textFight.parentElement.style.opacity = '1';
+	$textFight.parentElement.style.padding = '20px';
+	win_audio.play();
+	setTimeout(function(){ $textFight.parentElement.style.display = 'none'; }, 3000);
+}
+
+function showBoxLose() {
+	$textFight.parentElement.style.opacity = '1';
+	$textFight.parentElement.style.padding = '20px';
+	fail_audio.play();
+	setTimeout(function(){ $textFight.parentElement.style.display = 'none'; }, 3000);
+}
+
+//show the modal 
+function showModal(text) {
+  modalText.innerText = text;
+  modal.style.display = "block";
+  setTimeout(function(){ modal.style.display = 'none'; }, 5000);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 const box = {
 	name_bonus : function (){
-		alert('Открыт бонусный удар!!! Используй его чтобы победить! ✨');
+		showModal('Открыт бонусный удар!!! Используй его чтобы победить! ✨');
+		document.getElementById("canvas").style.visibility = "visible";
 	},
 	name_win : function (){
 		$textFight.innerHTML = 'ТЫ выиграл, WOW</br>' + character.name + ' win!';
-		boxThis();
+		showBoxWin();
 	},
 	name_lost : function (){
 		$textFight.innerHTML = 'ШТОШ, ты проиграл</br>' + enemy.name + ' win!';
-		boxThis();
+		showBoxLose();
 	},
 }
 
@@ -76,12 +101,6 @@ $btn_kick_bonus.addEventListener('click', function(){
 	$kick_bonus.classList.remove('control');
 	$kick_bonus.classList.add('none');
 })
-
-function init(){
-	console.log('Start Game!');
-	renderHP(character);
-	renderHP(enemy);
-}
 
 function kickBonus(){
 	if(!isResizeble){
@@ -123,6 +142,12 @@ function changeHP (count, person){
 
 function random(num){
 	return Math.ceil(Math.random() * num);
+}
+
+function init(){
+	console.log('Start Game!');
+	renderHP(character);
+	renderHP(enemy);
 }
 
 init();
